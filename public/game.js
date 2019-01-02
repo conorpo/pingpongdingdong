a2c7e7fa768a398fa24601fda29088a1fe441723
Conor <conor4arms@gmail.com>
2019-01-01 19:22:55 -0500

    V1

---
+++ public/game.js
@@ -0,0 +1,28 @@
+let socket = io();
+
+socket.on('connect', function(){
+  socket.emit('queue', deparam(window.location.search).name , function(err){
+    if(err){
+      alert(err);
+      window.location.href = "/";
+    }
+  })
+  socket.on('found', function(id, username){
+    document.write("Found a game with " + username + " in Game ID " + id);
+  })
+});
+
+function deparam(uri){
+    if(uri === undefined){
+      uri = window.location.search;
+    }
+    var queryString = {};
+    uri.replace(
+      new RegExp(
+        "([^?=&]+)(=([^&#]*))?", "g"),
+        function($0, $1, $2, $3) {
+        	queryString[$1] = decodeURIComponent($3.replace(/\+/g, '%20'));
+        }
+      );
+    return queryString;
+};
--- server/server.js
+++ server/server.js
@@ -12,10 +12,43 @@ const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+let people = [];
+let games = [];
+class Game{
+  constructor(personA, personB, id){
+    this.A = personA;
+    this.B = personB;
+    personA.join(newGame).emit("found",id, personB.username);
+    personB.join(newGame).emit("found",id, personA.username);
+    console.log("New Game between " + personA.username + " and " + personB.username);
+  }
+}
+
 io.on('connection',(socket) => {
-    console.log("New connectipn: " + socket.id);
+    console.log("New connection: " + socket.id);
+    socket.on("queue", (username, callback) => {
+      if(!isRealString(params.name)){
+            return callback('Name is required, and must be less than 20 chars long');
+      };
+      socket.username = username;
+      people.push(socket);
+      checkGame();
+    })
 });
+
+function checkGame(){
+  if(people.length > 1){
+    const newGame = games.length.toString();
+    const personA = people.shift()
+    const personB = people.shift();
+    games.push(new Game(personA, personB, newGame));
+  }
+}
 
 server.listen(port, () => {
     console.log("Listeing on port " + port);
 });
+
+let isRealString = (str) => {
+    return typeof str == "string" && str.trim().length > 0 && str.trim().length < 20;
+}
